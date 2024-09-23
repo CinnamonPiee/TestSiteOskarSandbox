3daac4747a91ab38412d3928994cd5ceeeea8147
CinnamonPie <vonahkahs.s.ved@gmail.com>
2024-09-23 13:16:09 +0400

    reformat to check element on page and fix test

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-from pages.main_page import MainPage
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -6,3 +6,4 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    page.should_be_login_link()
