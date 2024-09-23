8eca62d43148904c0a124b1fa93e54e0c492bec8
CinnamonPie <vonahkahs.s.ved@gmail.com>
2024-09-23 13:44:18 +0400

    add new test in login page

---
+++ test_login_page.py
@@ -0,0 +1,9 @@
+from .pages.login_page import LoginPage
+
+
+def test_quest_can_see_login_or_registration(browser):
+    link = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+    
